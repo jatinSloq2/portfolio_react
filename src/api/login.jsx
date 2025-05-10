37fcc15cb8c6c2d1743105a37c73908254330fc6
Jatin Singh <jatinsingh098loq2@outlook.com>
2025-05-10 15:21:37 +0530

    Update login.jsx
     console.log("Form submitted in", Date.now() - start, "ms");

--- src/api/login.jsx
+++ src/api/login.jsx
@@ -4,8 +4,10 @@ const api = axios.create({ baseURL: "https://backend-portfolio-react.onrender.co
 
 export const login = async (email, password) => {
   try {
-    const response = await api.post("/login", { email, password });
+      const start = Date.now();
+    const response = await api.post("/login", { email, password },  { timeout: 5000 });
     const token = response.data.token;
+     console.log("Form submitted in", Date.now() - start, "ms");
     return token;
   } catch (error) {
     console.error("Login failed:", error.response ? error.response.data : error.message);
