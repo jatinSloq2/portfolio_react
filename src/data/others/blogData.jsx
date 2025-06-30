5331a1b36fe74df09c1e6bee00d32cb48b23de9a
Jatin Singh <jatinsingh098loq2@outlook.com>
2025-06-30 21:11:30 +0530

    Resume Added in this

--- src/data/others/blogData.jsx
+++ src/data/others/blogData.jsx
@@ -3,6 +3,8 @@ import blog2 from "../blogs/blog02";
 // import blog3 from "../blogs/blog03";
 // import blog04 from "../blogs/blog04";
 
+
+
 const blogPosts = [ blog2, blog1];
 
 export default blogPosts
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,6 @@ const About = () => {
         />
 
         <div className="flex flex-col lg:flex-row justify-between items-start gap-10 mt-16">
-          {/* Left: Image of the person */}
           <div className="max-w-[300px] w-full mx-auto">
             <img
               src="images/WhatsApp Image 2025-06-14 at 18.31.40_501ad617.jpg"
@@ -29,7 +28,6 @@ const About = () => {
              Over time, my passion for problem-solving has evolved into a deep dedication to merging design with function. I've come to see every challenge as an opportunity to create experiences that are not only visually captivating but also highly intuitive. Crafting interfaces that balance beauty with usability has become my mission. Each project I take on feels like a blank canvas, where I meticulously shape ideas into seamless, purposeful designs. With every detail considered and every line of code written, I strive to bring visions to life with precision, care, and an unwavering sense of purpose. My education at Parishkar College of Global Excellence and full-stack developer training from Grass Institute have further solidified my foundation, allowing me to blend design and technology seamlessly.
             </p>
 
-            {/* Stats Section */}
             <div className="flex gap-8 flex-wrap mb-6">
               <div>
                 <h1 className="text-4xl lg:text-5xl font-bold text-emerald-600 dark:text-emerald-400">
@@ -51,10 +49,9 @@ const About = () => {
               </div> */}
             </div>
 
-            {/* Divider Line */}
             <div className="h-[2px] bg-gradient-to-r from-transparent via-emerald-950 dark:via-emerald-500 to-emerald-950 mb-6" />
 
-            {/* Contact Info */}
+
             <div className="flex flex-col lg:flex-row gap-x-5">
               <p className="text-base lg:text-md">
                 <span className="font-semibold text-emerald-600 dark:text-emerald-400">
